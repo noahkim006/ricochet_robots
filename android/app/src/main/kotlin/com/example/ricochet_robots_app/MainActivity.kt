0be12cd491a02e14fb1c307d9eb8c117ca36059c
noahkim006 <noahkim0981@gmail.com>
2024-04-25 13:58:06 -0700

    first

---
+++ android/app/src/main/kotlin/com/example/ricochet_robots_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ricochet_robots_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:ricochet_robots_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ricochet Robots',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  const MyHomePage(),
+    );
+  }
+}
+
+
