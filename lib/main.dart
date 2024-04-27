b29da1da97070405770b53c657e3f907c83dc950
unknown <noahkim0981@gmail.com>
2024-04-27 10:03:01 -0700

    pivoted to using flame for game with tutorial advice, set fullscreen and portrait for future gamepkay

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:ricochet_robots_app/ricochet_robots.dart';
+// ignore: unused_import
 import 'package:ricochet_robots_app/screens/home_screen.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
 void main() {
-  runApp(const MyApp());
-}
+  //wait for flutter to be init before call other stuff
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setPortrait();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ricochet Robots',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
-    );
-  }
+  RicochetRobots game = RicochetRobots();
+  runApp(GameWidget(game: game));
 }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Ricochet Robots',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
